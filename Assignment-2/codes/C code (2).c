8b921da2838ea0dab842c1aa83d36bf48fd0a029
SriCharvi <103203283+SriCharvi@users.noreply.github.com>
2022-05-17 01:13:59 +0530

    Add files via upload

---
+++ Assignment-2/codes/C code (2).c
@@ -0,0 +1,21 @@
+/*
+  ICSE 2019 Grade 12
+Question 15(a)
+Name: Sri Charvi Salapu
+Roll number: BT21BTECH11008
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    float a,b,c;
+    a=5;
+    c=13;
+    b = sqrt((c*c)-(a*a));
+    printf("%.f",b);
+    
+    
+    return 0;
+}
