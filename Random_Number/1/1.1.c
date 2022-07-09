a2131ca775690a68a852a6a6b161dbf9de330600
SriCharvi <charvisalapu@gmail.com>
2022-07-09 13:55:14 -0400

    Random number

---
+++ Random_Number/1/1.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../header.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+
+return 0;
+}
+
+
