d1c19bb3d30e50c50286bb420adf0157c2c5b1fa
SriCharvi <charvisalapu@gmail.com>
2022-07-11 08:39:23 -0400

    update

--- Random_Number/2/2.2.py
+++ Random_Number/2/2.2.py
@@ -1,49 +1,48 @@
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import mpmath as mp
 import scipy
 
 #if using termux
+
 #import subprocess
 #import shlex
+
 #end if
 
 
 
-x = np.linspace(-4,4,100)#points on the x axis
+x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
+#randvar = np.loadtxt('uni.dat',dtype='double')
 randvar = np.loadtxt('gau.dat',dtype='double')
-#randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,100):
-	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-	err_n = np.size(err_ind) #computing the probability
-	err.append(err_n/simlen) #storing the probability values in a list
+for i in range(0,30):
+        err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+        err_n = np.size(err_ind) #computing the probability
+        err.append(err_n/simlen) #storing the probability values in a list
+
+def Q(x):
+        return 0.5*(1-mp.erf(x/mp.sqrt(2)))
 
 def cdf(x):
-	if x < 0:
-		return 0.0
-	elif x>=0 and x<=1:
-		return x
-	else:
-		return 1.0
+        return 1 - Q(x)
 
-vec_gau_cdf = scipy.vectorize(cdf)
-
-	
-plt.plot(x.T,err,'o')#plotting the CDF
-plt.plot(x,vec_gau_cdf(x))#plotting the CDF
+vec_cdf = scipy.vectorize(cdf,otypes=[np.float64])
+plt.plot(x.T,err,"o")#plotting the CDF
+plt.plot(x,vec_cdf(x))
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
 
 #if using termux
-plt.savefig('../figs/gau_cdf.pdf')
-#plt.savefig('../figs/gau_cdf.eps')
+#plt.savefig('./figs/uni_cdf.pdf')
+#plt.savefig('./figs/uni_cdf.eps')
 #subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
 #if using termux
-#plt.savefig('../figs/gauss_cdf.pdf')
+plt.savefig('../figs/gauss_cdf.pdf')
 #plt.savefig('../figs/gauss_cdf.eps')
 #subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
 #else
